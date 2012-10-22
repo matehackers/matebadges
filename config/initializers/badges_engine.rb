02cf10ccc1ceb1eb4e0b9d64e948476665994f17
Lucas Zawacki <lfzawacki@gmail.com>
2012-10-22 16:36:50 -0200

    Corrected configuration file

--- config/initializers/badges_engine.rb
+++ config/initializers/badges_engine.rb
@@ -1,13 +1,13 @@
 BadgesEngine::Configuration.setup do |config|
   config.issuer = {
-    :origin => 'http://badges.matehackers.com',
+    :origin => 'http://badges.matehackers.org',
     :name => 'Courses',
     :org => 'Matehackers',
     :contact => 'courses@matehackers.org'
   }
 
   config.baker_url = 'http://beta.openbadges.org/baker'
-  config.badge_version = '0.5.0'
+  config.badge_version = '1.0'
   config.user_class = User
   config.salt = 'poopbadges'
 end
