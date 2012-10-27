3257b6e230c56c5f53d7e7f3023ea6857ac4c6ca
Lucas Zawacki <lfzawacki@gmail.com>
2012-10-27 15:23:47 -0200

    Let's send gmail emails

--- app/mailers/badge_mailer.rb
+++ app/mailers/badge_mailer.rb
@@ -1,6 +1,8 @@
 class BadgeMailer < ActionMailer::Base
   default from: "courses@matehackers.org"
 
+  add_template_helper(ApplicationHelper)
+
   def badge_email(assertion)
     @user = assertion.user
     @title = assertion.badge.name
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,11 +61,22 @@ Matebadges::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'badges.matehackers.org' }
+  # config.action_mailer.default_url_options = { :host => 'badges.matehackers.org' }
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'matehackers.org',
+    :user_name            => IO.read('mail_user'),
+    :password             => IO.read('mail_password'),
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
